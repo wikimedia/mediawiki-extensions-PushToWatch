a72d245832cb1d51581599c6039112a5f6cb3a7e
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2020-11-23 17:26:12 +0100

    Fix all PHPCS issues, and more
    I don't think this code works. Still I tried my best to fix
    the most obvious issues:
    * The indention was messed up.
    * I removed multiple unused variables.
    * The final `return $output` was obviously misplaced.
    * The hook handler doesn't need to return true.
    * No need to pass objects by reference.
    
    Change-Id: Ic55cf5ac504837248661a5a0d098cbe4bdc06dd1

--- src/PushToWatch.php
+++ src/PushToWatch.php
@@ -4,35 +4,32 @@ class PushToWatch {
 
 	/**
 	 * @param Title $title
-	 * @param User $user
+	 * @param string $user
 	 */
 	private static function addtoWatch( $title, $user ) {
 		global $wgNoReplyAddress, $wgUser;
 
 		$user = User::newFromName( $user );
-			if ( !is_object( $user ) || $user->getID() == 0 ) {
-				throw new Exception( "Invalid user lookup" );
-			}
+		if ( !is_object( $user ) || $user->getID() == 0 ) {
+			throw new Exception( "Invalid user lookup" );
+		}
 
 		if ( $user->isWatched( $title ) ) {
 			return;
 		}
 
-		$res = $user->addWatch( $title );
-
-		$medit = false;
+		$user->addWatch( $title );
 
 		$to = new MailAddress( $user->getEmail(), $user->getName(), $user->getRealName() );
 		$from = new MailAddress( $wgUser->getEmail(), $wgUser->getName(), $wgUser->getRealName() );
 		$replyto = new MailAddress( $wgNoReplyAddress );
 
-		$pagename = $title->getPrefixedText();
 		$pageurl = $title->getFullUrl();
 
 		$username = $user->getRealName();
-		$wgUsername = $wgUser->getRealName();
+		$currentUser = $wgUser->getRealName();
 
-		$body = "Hi $username,\r\n$wgUsername requested you to watch $pageurl\r\nCongrats !";
+		$body = "Hi $username,\r\n$currentUser requested you to watch $pageurl\r\nCongrats !";
 		$subject = "Watchlist injection - $title";
 
 		UserMailer::send( [ $to, $from ], $from, $subject, $body, [ 'replyTo' => $replyto ] );
@@ -64,39 +61,41 @@ class PushToWatch {
 			$output = "No follower";
 
 			if ( $res->numRows() ) {
+				$users = [];
+				foreach ( $res as $row ) {
+					$users[] = $row->user_real_name;
 
-			$users = [];
-			foreach ( $res as $row ) {
-				$users[] = $row->user_real_name;
+					$output = 'Followers : ' . implode( ', ', $users ) . '.';
+				}
 
-			$output = 'Followers : ' . implode( ', ', $users ) . '.';
+				$output .= Html::rawElement( 'form', [ 'method' => 'POST' ],
+					'Push to watch : ' .
+					Html::submitButton( '', [ 'style' => 'display:none' ] ) .
+					Html::input( 'pushtowatch_user' )
+				);
 			}
-
-			$output .= "<form method='POST'>Push to watch : <input type='submit' style='display:none'/><input type='text' name='pushtowatch_user'/></form>";
-
-			}
-		}
-		catch ( Exception $e ) {
+		} catch ( Exception $e ) {
 			error_log( 'Wiki, follower error :' . $e->getMessage() );
-
-			return $output;
 		}
+
+		return $output;
 	}
 
 	/**
 	 * @param SkinTemplate $sk
-	 * @param QuickTemplate &$tpl
-	 * @return bool
+	 * @param QuickTemplate $tpl
 	 */
-	public static function listUsers( $sk, &$tpl ) {
+	public static function listUsers( $sk, $tpl ) {
 		$title = $sk->getRelevantTitle();
 		$output = "<hr/>";
 
 		try {
-			$user = preg_replace( "#[^a-z]#i", "", $_POST['pushtowatch_user'] );
-		if ( $user ) {
-			self::addtoWatch( $title, $user );
-		}
+			$user = $sk->getRequest()->getText( 'pushtowatch_user' );
+			// FIXME: This destroys all usernames that contain other characters!
+			$user = preg_replace( "#[^a-z]#i", "", $user );
+			if ( $user ) {
+				self::addtoWatch( $title, $user );
+			}
 		} catch ( Exception $e ) {
 			$output .= "<div class='error'>Could not add <b>$user</b> to watchlist</div>";
 		}
@@ -105,6 +104,5 @@ class PushToWatch {
 
 		$tpl->set( 'followerList',  $output );
 		$tpl->data['footerlinks']['info'][] = 'followerList';
-		return true;
 	}
 }
